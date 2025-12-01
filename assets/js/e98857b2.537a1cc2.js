d09efccc92b3d7e6a0b0b20190b0ca9d12dcd66e
David <davidslice619@gmail.com>
2025-12-01 19:55:23 +0100

    Deploy website - based on 1716d6aef216f9cedb448ea7f6f94239077c08ce

---
+++ assets/js/e98857b2.537a1cc2.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmy_website=globalThis.webpackChunkmy_website||[]).push([[7053],{74013:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/docstest2/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
